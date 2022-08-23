3700b429f151cee88de5fb75d37110eee2dbdc22
Vitória Moura <vim.saturnino@gmail.com>
2022-08-22 22:25:44 -0300

    exemplo antipattern

---
+++ engenharia3/estrategia/antipattern/Arara.java
@@ -0,0 +1,10 @@
+package antipattern;
+
+public class Ave {
+	
+	public void voar(String especie) {
+		System.out.println("Oii eu sou um(a) " + especie + " e estou voando! Ao infinito e além");
+	}
+	
+}
+ 
